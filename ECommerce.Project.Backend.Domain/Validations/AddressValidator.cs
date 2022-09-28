248d31382a2962f871c3c718ec66764469dd5bbc
LuisNataan <luisnataan556@gmail.com>
2022-09-28 14:26:17 -0300

    Develop: Creating Address and Supplier validators.

---
+++ ECommerce.Project.Backend.Domain/Validations/AddressValidator.cs
@@ -0,0 +1,53 @@
+﻿using ECommerce.Project.Backend.Domain.ComplexTypes;
+using ECommerce.Project.Backend.Domain.Utils;
+using FluentValidation;
+using System.Text.RegularExpressions;
+
+namespace ECommerce.Project.Backend.Domain.Validations
+{
+    public class AddressValidator : AbstractValidator<Address>
+    {
+        private readonly string _stringRule = "^[0 - 9 a - zA - ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ] +$";
+        private readonly string _stateAbbreviationRule = "^[A - Z]+$";
+        private readonly string _numberRule = "^[0 - 9] + $";
+        private readonly string _usZipCode = @"^\d{5}(?:[-\s]\d{4})?$";
+
+        public AddressValidator()
+        {
+            RuleFor(a => a.City)
+                .NotEmpty().WithMessage("City cannot be empty.")
+                .Length(4, 60).WithMessage("City must be 4 to 60 characters long.")
+                .Matches(_numberRule).WithMessage("City has invalid characters.");
+
+            RuleFor(a => a.State)
+                .NotEmpty().WithMessage("State cannot be empty.")
+                .Length(2, 2).WithMessage("State is 2 characters long.")
+                .Matches(_stateAbbreviationRule).WithMessage("State has invalid characters.");
+
+            RuleFor(a => a.StreetName)
+                .NotEmpty().WithMessage("Street name cannot be empty.")
+                .Length(4, 60).WithMessage("Street name must be 4 to 60 characters long.")
+                .Matches(_stringRule).WithMessage("Street name has invalid characters.");
+
+            RuleFor(a => a.Number)
+                .NotEmpty().WithMessage("Number cannot be empty.")
+                .Length(1, 99999)
+                .Matches(_numberRule).WithMessage("Number has invalid characters.");
+
+            RuleFor(a => a.ZipCode)
+                .NotEmpty().WithMessage("Zip Code cannot be empty.")
+                .Length(6, 6).WithMessage("Zip Code must be 6 characters long.")
+                .Must(IsValidZipCode).WithMessage("Invalid Zip Code.");
+        }
+
+        internal bool IsValidZipCode(string zipCode)
+        {
+            var validZipCode = true;
+            if (!Regex.Match(zipCode, _usZipCode).Success)
+            {
+                validZipCode = false;
+            }
+            return validZipCode;
+        }
+    }
+}
---
+++ ECommerce.Project.Backend.Domain/Validations/SupplierValidation.cs
@@ -0,0 +1,37 @@
+﻿using ECommerce.Project.Backend.Domain.Entities;
+using FluentValidation;
+
+namespace ECommerce.Project.Backend.Domain.Validations
+{
+    public class SupplierValidation : AbstractValidator<Supplier>
+    {
+        private readonly string _supplierName = "^[0 - 9 a - zA - ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ] +$";
+        private readonly string _numberRule = "^[0 - 9] + $";
+
+        public SupplierValidation()
+        {
+            RuleFor(s => s.Name)
+                .NotEmpty().WithMessage("Name cannot be empty.")
+                .Length(4, 50).WithMessage("Name must be 4 to 50 characters long.")
+                .Matches(_supplierName).WithMessage("Name has invalid characters.");
+
+            RuleFor(s => s.CorporateName)
+                .NotEmpty().WithMessage("Corporate Name cannot be empty.")
+                .Length(4, 50).WithMessage("Corporate Name must be 4 to 50 characters long.")
+                .Matches(_supplierName).WithMessage("Corporate Name has invalid characters");
+
+            RuleFor(s => s.PhoneNumber)
+                .NotEmpty().WithMessage("Phone number cannot be empty.")
+                .Length(11, 11).WithMessage("Phone number mus be 11 characters long.")
+                .Matches(_numberRule).WithMessage("Phone number has invalid characters.");
+            
+            RuleFor(s => s.Email)
+                .NotEmpty().WithMessage("Email cannot be empty.")
+                .Length(12, 60).WithMessage("Email must be 12 to 60 characters long.")
+                .EmailAddress().WithMessage("Invalid email address. Ex: example@examroom.ai");
+
+            RuleFor(s => s.EinNumber)
+                .NotEmpty().WithMessage("EIN Number cannot be empty.");
+        }
+    }
+}
