c1fff51ca3f8a376fdd4c11f2091140750784698
LuisNataan <luisnataan556@gmail.com>
2022-09-28 18:16:23 -0300

    Develop: Creating Respositries.

---
+++ ECommerce.Project.Backend.Infra/Repositories/SupplierRepository.cs
@@ -0,0 +1,34 @@
+﻿using ECommerce.Project.Backend.Domain.Entities;
+using ECommerce.Project.Backend.Domain.Interfaces;
+using ECommerce.Project.Backend.Infra.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Project.Backend.Infra.Repositories
+{
+    public class SupplierRepository : GenericRepository<Supplier>, ISupplierRepository
+    {
+        public SupplierRepository(MainContext context) : base(context)
+        {
+        }
+
+        public override async Task<Supplier> GetById(int id) 
+        {
+            return await _dbSet.Include(s => s.Adress).FirstOrDefaultAsync(s => s.Id == id && !s.Deleted);
+        }
+
+        public async Task<Supplier> GetSupplier(string einNumber)
+        {
+            return await Query().FirstOrDefaultAsync(s => s.EinNumber == einNumber);
+        }
+
+        public async Task<bool> SameEinNumberSupplier(string einNumber, int id)
+        {
+            return await Query().AnyAsync(s => s.EinNumber == einNumber && s.Id == id && !s.Deleted);
+        }
+
+        public async Task<bool> SupplierAlreadyExists(string einNumber)
+        {
+            return await Query().AnyAsync(s => s.EinNumber == einNumber && !s.Deleted);
+        }
+    }
+}
