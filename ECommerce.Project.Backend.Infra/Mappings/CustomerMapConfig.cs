a7e64d4c45cc7b2aa0af693991566395356b6cb9
LuisNataan <luisnataan556@gmail.com>
2022-09-29 23:31:27 -0300

    Develop: Creating Mapping Configuration

---
+++ ECommerce.Project.Backend.Infra/Mappings/CustomerMapConfig.cs
@@ -0,0 +1,28 @@
+﻿using ECommerce.Project.Backend.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ECommerce.Project.Backend.Infra.Mappings
+{
+    internal class CustomerMapConfig : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.ToTable("Customers");
+
+            
+            builder.Property(c => c.Name)
+                .HasMaxLength(60)
+                .IsRequired();
+
+            builder.Property(c => c.Email)
+                .HasMaxLength(60)
+                .IsRequired();
+
+            builder.Property(c => c.PhoneNumber)
+                .IsFixedLength()
+                .HasMaxLength(11)
+                .IsRequired();
+        }
+    }
+}
---
+++ ECommerce.Project.Backend.Infra/Mappings/SupplierMapConfig.cs
@@ -0,0 +1,36 @@
+﻿using ECommerce.Project.Backend.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ECommerce.Project.Backend.Infra.Mappings
+{
+    public class SupplierMapConfig : IEntityTypeConfiguration<Supplier>
+    {
+        public void Configure(EntityTypeBuilder<Supplier> builder)
+        {
+            builder.ToTable("Suppliers");
+
+            builder.Property(s => s.Name)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(s => s.CorporateName)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(s => s.EinNumber)
+                .IsFixedLength()
+                .HasMaxLength(9)
+                .IsRequired();
+
+            builder.Property(s => s.PhoneNumber)
+                .IsFixedLength()
+                .HasMaxLength(11)
+                .IsRequired();
+
+            builder.Property(s => s.Email)
+                .HasMaxLength(60)
+                .IsRequired();
+        }
+    }
+}
