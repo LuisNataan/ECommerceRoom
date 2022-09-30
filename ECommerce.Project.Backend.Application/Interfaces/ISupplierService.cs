073550286d22a5088473905a208bb7d7b8203b4f
LuisNataan <luisnataan556@gmail.com>
2022-09-30 11:21:09 -0300

    Develop: Refactoring ISupplierService

--- ECommerce.Project.Backend.Application/Interfaces/ISupplierService.cs
+++ ECommerce.Project.Backend.Application/Interfaces/ISupplierService.cs
@@ -7,7 +7,7 @@ namespace ECommerce.Project.Backend.Application.Interfaces
         Task Create(Supplier supplier);
         Task Delete(Supplier supplier);
         Task Update(Supplier supplier);
-        Task<IEnumerable<Supplier>> GetAll(Supplier supplier);
+        Task<IEnumerable<Supplier>> GetAll();
         Task<Supplier> GetById(int id);
         Task<bool> SameEinNumberSupplier(string einNumber, int id);
         Task<bool> SupplierAlreadyExists(string einNumber);
