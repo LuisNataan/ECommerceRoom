28d479e6b7b2694a8643bc0433bfc47bddfdf306
LuisNataan <luisnataan556@gmail.com>
2022-09-30 11:22:42 -0300

    Develop: Configuring Program class.

--- ECommerceRoom/Program.cs
+++ ECommerceRoom/Program.cs
@@ -1,12 +1,31 @@
+using ECommerce.Project.Backend.Application.Interfaces;
+using ECommerce.Project.Backend.Application.Services;
+using ECommerce.Project.Backend.Domain.Interfaces;
+using ECommerce.Project.Backend.Infra.Context;
+using ECommerce.Project.Backend.Infra.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContextPool<MainContext>(options => options.UseSqlServer("ConnectionStrings"));
+
+builder.Services.AddTransient<ISupplierService, SupplierService>();
+builder.Services.AddTransient<ISupplierRepository, SupplierRepository>();
+
+builder.Services.AddTransient<ICustomerService, CustomerService>();
+builder.Services.AddTransient<ICustomerRepository, CustomerRepository>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -19,6 +38,13 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
 
 app.MapRazorPages();
 
