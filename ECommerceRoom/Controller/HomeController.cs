bd6127d67827e2b482fe2da599bd7f1b5e385c02
LuisNataan <luisnataan556@gmail.com>
2022-09-30 14:48:35 -0300

    Develop: Creating Home and SupplierController.

---
+++ ECommerceRoom/Controller/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using ECommerce.Project.Backend.Application.Interfaces;
+using ECommerce.Project.Backend.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace ECommerce.Project.Backend.Web.Controller
+{
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+        private ISupplierService _supplierService;
+        private ICustomerService _customerService;
+
+        public HomeController(ILogger<HomeController> logger, ISupplierService supplierService, ICustomerService customerService)
+        {
+            _logger = logger;
+            _supplierService = supplierService;
+            _customerService = customerService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                List<Supplier> suppliers = await _supplierService.GetAll();
+                List<Customer> customers = await _customerService.GetAll();
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ ECommerceRoom/Controller/SupplierController.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using ECommerce.Project.Backend.Application.Commom;
+using ECommerce.Project.Backend.Application.Interfaces;
+using ECommerce.Project.Backend.Domain.Entities;
+using ECommerce.Project.Backend.Domain.Models.Insert;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace ECommerce.Project.Backend.Domain.Controller
+{
+    public class SupplierController : ControllerBase
+    {
+        private readonly ISupplierService _supplier;
+
+        public SupplierController(ISupplierService supplier)
+        {
+            _supplier = supplier;
+        }
+
+
+        public IActionResult Index()
+        {
+            return Content("Welcome to the ECommerce!");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(SupplierInsertViewModel supplierViewModel)
+        {
+            try
+            {
+                var config = new MapperConfiguration(s => 
+                {
+                    s.CreateMap<SupplierInsertViewModel, Supplier>();
+                });
+                IMapper mapper = config.CreateMapper();
+                Supplier supplier = mapper.Map<Supplier>(supplierViewModel);
+                await _supplier.Create(supplier);
+
+                return Ok();
+            }
+            catch (Exceptions ex)
+            {
+                return UnprocessableEntity(ex.Message);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+
+    }
+}
