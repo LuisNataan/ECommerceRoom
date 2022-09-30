ef28a47baa4ac4f6a39d4be399c060f3cd0596da
LuisNataan <luisnataan556@gmail.com>
2022-09-30 14:49:17 -0300

    Develop: Creating SupplierInsertViewModel class.

---
+++ ECommerceRoom/Models/Insert/SupplierInsertViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ECommerce.Project.Backend.Domain.ComplexTypes;
+
+namespace ECommerce.Project.Backend.Domain.Models.Insert
+{
+    public class SupplierInsertViewModel
+    {
+        public string Name { get; set; }
+        public string CorporateName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string EinNumber { get; set; }
+        public Address Address { get; set; }
+    }
+}
