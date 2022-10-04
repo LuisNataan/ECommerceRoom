3b62340e55263f001101cdf7f3de76a135fa0503
LuisNataan <luisnataan556@gmail.com>
2022-10-04 09:37:10 -0300

    Develop: Changing Address property.

--- ECommerceRoom/Models/Insert/CustomerInsertViewModel.cs
+++ ECommerceRoom/Models/Insert/CustomerInsertViewModel.cs
@@ -5,6 +5,6 @@
         public string Name { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
-        public AddressInsertViewModel Address { get; set; }
+        public AddressInsertViewModel AddressViewModel { get; set; }
     }
 }
