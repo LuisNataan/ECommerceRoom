ff25e009695db5632b795cd79b569d8bae048771
LuisNataan <luisnataan556@gmail.com>
2022-10-04 12:00:54 -0300

    Develop: Refactoring Supplier Service interface.

--- ECommerce.Project.Backend.Application/Interfaces/ISupplierService.cs
+++ ECommerce.Project.Backend.Application/Interfaces/ISupplierService.cs
@@ -5,7 +5,7 @@ namespace ECommerce.Project.Backend.Application.Interfaces
     public interface ISupplierService 
     {
         Task Create(Supplier supplier);
-        Task Delete(Supplier supplier);
+        Task Delete(int id);
         Task Update(Supplier supplier);
         Task<List<Supplier>> GetAll();
         Task<Supplier> GetById(int id);
