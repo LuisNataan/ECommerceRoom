0288ca393bae7729254ce3a79cf1846c6790649f
LuisNataan <luisnataan556@gmail.com>
2022-10-10 20:51:25 -0300

    Develop: Creating Signal Hub class.

---
+++ ECommerceRoom/Utils/Signal Hub/SignalHub.cs
@@ -0,0 +1,18 @@
+﻿using ECommerce.Project.Backend.Domain.Entities;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ECommerce.Project.Backend.Web.Utils.Signal_Hub
+{
+    public class SignalHub : Hub
+    {
+        public void NotificationMessage(string message) 
+        {
+            Clients.All.SendAsync("NotificationMessage", message);
+        }
+
+        public void SendNotification(Customer customer) 
+        {
+            Clients.All.SendAsync("SendNotification", customer);
+        }
+    }
+}
